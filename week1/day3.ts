1ff864bd30f351c875db04e4aca276c0fa9d1640
Chris May <chris.may@bjss.com>
2020-12-03 10:08:50 +0000

    renames

--- week1/day3.ts
+++ week1/day3.ts
@@ -9,15 +9,15 @@ type Move = {
 // Holds our progress through the map.
 type Progress = {
   xPos: number; // How far right have we travelled so far?
-  countTrees: number; // how many trees have we seen so far?
-  skipCount: number; // Do we need to skip this line (e.g. because we're doing "down 2")?
+  treeCount: number; // how many trees have we seen so far?
+  skipCountDown: number; // Do we need to skip this line (e.g. because we're doing "down 2")?
 };
 
 const initProgress: Progress = {
   // always start in the top left corner.
   xPos: 0,
-  countTrees: 0,
-  skipCount: 0, // never skip the first line.
+  treeCount: 0,
+  skipCountDown: 0, // never skip the first line.
 };
 
 export function day3() {
@@ -27,18 +27,18 @@ export function day3() {
 
   const lines = skiMap.split("\n");
 
-  function nextMove(move: Move) {
+  function processLine(move: Move) {
     return function (progress: Progress, line: string): Progress {
-      if (progress.skipCount > 0) {
+      if (progress.skipCountDown > 0) {
         // We're doing "Down 2" or something like that, so skip this line and decrement the skip counter.
-        return { ...progress, skipCount: progress.skipCount - 1 };
+        return { ...progress, skipCountDown: progress.skipCountDown - 1 };
       } else {
         // We're on a line that we need to look for trees on
         const isTree = charAt(line, progress.xPos) === "#";
         return {
           xPos: progress.xPos + move.right,
-          countTrees: progress.countTrees + (isTree ? 1 : 0),
-          skipCount: move.down - 1,
+          treeCount: progress.treeCount + (isTree ? 1 : 0),
+          skipCountDown: move.down - 1,
         };
       }
     };
@@ -46,8 +46,8 @@ export function day3() {
 
   const part1Move = { right: 3, down: 1 };
 
-  const part1Result = lines.reduce(nextMove(part1Move), initProgress);
-  console.log("Day 3 Part 1:", part1Result.countTrees);
+  const part1Result = lines.reduce(processLine(part1Move), initProgress);
+  console.log("Day 3 Part 1:", part1Result.treeCount);
 
   const part2Moves: Move[] = [
     { right: 1, down: 1 },
@@ -58,8 +58,8 @@ export function day3() {
   ];
 
   const part2Result = part2Moves
-    .map((move) => lines.reduce(nextMove(move), initProgress))
-    .reduce((prod, progress) => prod * progress.countTrees, 1); //part 2 is only interested in the product of all the tree-counts.
+    .map((move) => lines.reduce(processLine(move), initProgress))
+    .reduce((prod, progress) => prod * progress.treeCount, 1); //part 2 is only interested in the product of all the tree-counts.
 
   console.log("Day 3 part 2:", part2Result);
 }
