20be7e7d0b81f9a03ecdac3ef9146d6aafad6c72
Chris May <chris.may@bjss.com>
2020-12-05 18:17:50 +0000

    Replace the chained replace() with a map+test

--- week1/day5.ts
+++ week1/day5.ts
@@ -2,15 +2,19 @@ import { difference, differenceBy, max, min, range, sortBy, zip } from "lodash";
 import { tickets } from "./day5_inputs";
 
 function seatID(ticket: string): number {
-  return parseInt(ticket.replace(/[FL]/g, "0").replace(/[BR]/g, "1"), 2);
+  return parseInt(
+    [...ticket].map((m) => (/[BR]/.test(m) ? "1" : "0")).join(""),
+    2
+  );
 }
 
 export function day5(): void {
   const allocatedSeats = sortBy([...tickets.split("\n").map(seatID)]);
-  const lowestSeat = min(allocatedSeats) || 0;
   const highestSeat = max(allocatedSeats) || 0;
-  const allPossibleSeats = range(lowestSeat, highestSeat + 1);
+  console.log("Day 5 Part 1:", highestSeat);
 
+  const lowestSeat = min(allocatedSeats) || 0;
+  const allPossibleSeats = range(lowestSeat, highestSeat + 1);
   const [firstAvailableSeat] = difference(allPossibleSeats, allocatedSeats);
 
   console.log("Day 5 Part 2:", firstAvailableSeat);
