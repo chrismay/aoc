37c78982a8446952ce9c943a6feed4ce2bfe3e2a
Chris May <chris.may@bjss.com>
2020-12-05 08:54:29 +0000

    Simplify. Durr

--- week1/day5.ts
+++ week1/day5.ts
@@ -1,14 +1,8 @@
 import { max, min, range, sortBy, zip } from "lodash";
 import { tickets } from "./day5_inputs";
 
-function toNumber(s: string): number {
-  return parseInt(s.replace(/[FL]/g, "0").replace(/[BR]/g, "1"), 2);
-}
-const ticketPattern = /^([BF]{7})([LR]{3})$/;
-
 function seatID(ticket: string): number {
-  const [, row, col] = ticket.match(ticketPattern) || [];
-  return toNumber(row) * 8 + toNumber(col);
+  return parseInt(ticket.replace(/[FL]/g, "0").replace(/[BR]/g, "1"), 2);
 }
 
 export function day5(): void {
