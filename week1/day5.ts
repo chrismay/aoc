eeeb22e2c8f8c83ee9e5e60568bd05d229c4cbd9
Chris May <chris.may@bjss.com>
2020-12-05 22:35:40 +0000

    refactor recursive search into a partial reducer implementation

--- week1/day5.ts
+++ week1/day5.ts
@@ -1,5 +1,19 @@
-import { head, max, min, sortBy, tail } from "lodash";
+import { sortBy } from "lodash";
 import { tickets } from "./day5_inputs";
+
+// reduce that will exit early if the reducing function ever returns undefined
+// in which case, the last value of the accumulator is returned and the remainder
+// of the list is ignored.
+function partialReduce<T, A>(
+  arr: T[],
+  f: (a: A, t: T) => A | undefined,
+  seed: A
+): A {
+  if (arr === []) return seed;
+  const [h, ...t] = arr;
+  const acc = f(seed, h);
+  return acc === undefined ? seed : partialReduce(t, f, acc);
+}
 
 function seatId(ticket: string): number {
   return [...ticket].reduce((id, c) => (id << 1) + (/[BR]/.test(c) ? 1 : 0), 0);
@@ -7,15 +21,18 @@ function seatId(ticket: string): number {
 
 export function day5(): void {
   const allocatedSeats = sortBy(tickets.split("\n").map(seatId));
-  const highestSeat = max(allocatedSeats) || 0;
+  const highestSeat = allocatedSeats[allocatedSeats.length - 1];
 
   console.log("Day 5 Part 1:", highestSeat);
 
-  const lowestSeat = min(allocatedSeats) || 0;
+  const lowestSeat = allocatedSeats[0];
 
-  function findFreeSeat(seats: number[], curr: number): number {
-    return head(seats) !== curr ? curr : findFreeSeat(tail(seats), ++curr);
-  }
-
-  console.log("Day 5 Part 2:", findFreeSeat(allocatedSeats, lowestSeat));
+  console.log(
+    "Day 5 Part 2:",
+    partialReduce(
+      allocatedSeats,
+      (curr, alloc) => (curr === alloc ? ++curr : undefined),
+      lowestSeat
+    )
+  );
 }
