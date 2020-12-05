02ce695cca3fad8493b545478028d4f78440ad5f
Chris May <chris.may@bjss.com>
2020-12-05 18:39:35 +0000

    remove dependency on lodash difference

--- week1/day5.ts
+++ week1/day5.ts
@@ -1,21 +1,21 @@
-import { difference, differenceBy, max, min, range, sortBy, zip } from "lodash";
+import { max, min, sortBy } from "lodash";
 import { tickets } from "./day5_inputs";
 
-function seatID(ticket: string): number {
-  return parseInt(
-    [...ticket].map((m) => (/[BR]/.test(m) ? "1" : "0")).join(""),
-    2
-  );
+function seatId(ticket: string): number {
+  return [...ticket].reduce((id, c) => (id << 1) + (/[BR]/.test(c) ? 1 : 0), 0);
 }
 
 export function day5(): void {
-  const allocatedSeats = sortBy(tickets.split("\n").map(seatID));
+  const allocatedSeats = sortBy(tickets.split("\n").map(seatId));
   const highestSeat = max(allocatedSeats) || 0;
+
   console.log("Day 5 Part 1:", highestSeat);
 
   const lowestSeat = min(allocatedSeats) || 0;
-  const allPossibleSeats = range(lowestSeat, highestSeat + 1);
-  const [firstAvailableSeat] = difference(allPossibleSeats, allocatedSeats);
 
-  console.log("Day 5 Part 2:", firstAvailableSeat);
+  const firstFreeSeat =
+    lowestSeat +
+    allocatedSeats.findIndex((seatId, idx) => seatId !== idx + lowestSeat);
+
+  console.log("Day 5 Part 2:", firstFreeSeat);
 }
