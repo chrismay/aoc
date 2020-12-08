79ae3f256e532f493ba374ee0bcef88173683fc1
Chris May <chris.may@bjss.com>
2020-12-08 10:58:15 +0000

    Use a while util to avoid recursion. Refactor program end guards out of instruction execution

---
+++ util.ts
@@ -0,0 +1,14 @@
+// hide mutable
+export function doWhile<T>(
+  f: (t: T) => T,
+  test: (t: T) => boolean,
+  init: T
+): T {
+  let ret = init;
+  let carryOn = test(init);
+  while (carryOn) {
+    ret = f(ret);
+    carryOn = test(ret);
+  }
+  return ret;
+}
--- week2/day8.ts
+++ week2/day8.ts
@@ -1,43 +1,73 @@
+import { doWhile } from "../util";
 import { day8Program, normalExit, part1Test } from "./day8_input";
 
 type ProgramState = {
   instructionPointer: number;
   accumulator: number;
-  visitedInstructions: { [address: number]: boolean };
   halt: "NO" | "OK" | "ERR";
 };
 
+type TrackingProgramState = ProgramState & {
+  visitedInstructions: { [address: number]: boolean };
+};
+
 function modifiedPrograms(program: string[]): string[][] {
-  const jmpToNop = program
-    .map((line, idx) => {
-      const [opcode, arg] = line.split(" ");
-      if (opcode !== "jmp") {
-        return [];
-      } else {
-        return Object.assign([], program, { [idx]: "nop " + arg });
-      }
-    })
-    .filter((prog) => prog.length === program.length);
-
-  const nopToJmp = program
-    .map((line, idx) => {
-      const [opcode, arg] = line.split(" ");
-      if (opcode !== "nop") {
-        return [];
-      } else {
-        return Object.assign([], program, { [idx]: "jmp " + arg });
-      }
-    })
-    .filter((prog) => prog.length === program.length);
-
-  return [...nopToJmp, ...jmpToNop];
+  function mapOpCode(from: string, to: string): string[][] {
+    return program
+      .map((line, idx) => {
+        const [opcode, arg] = line.split(" ");
+        if (opcode !== from) {
+          return [];
+        } else {
+          return Object.assign([], program, { [idx]: to + " " + arg });
+        }
+      })
+      .filter((prog) => prog.length === program.length);
+  }
+  return [...mapOpCode("nop", "jmp"), ...mapOpCode("jmp", "nop")];
 }
 
 export function day8() {
-  function instruction(program: string[], state: ProgramState): ProgramState {
-    if (state.instructionPointer === program.length) {
-      return { ...state, halt: "OK" };
+  function executeInstruction(
+    program: string[],
+    state: ProgramState
+  ): ProgramState {
+    const [opCode, arg] = program[state.instructionPointer].split(" ");
+    switch (opCode) {
+      case "nop":
+        return {
+          ...state,
+          instructionPointer: 1 + state.instructionPointer,
+        };
+      case "acc":
+        return {
+          ...state,
+          accumulator: state.accumulator + +arg,
+          instructionPointer: 1 + state.instructionPointer,
+        };
+      case "jmp":
+        return {
+          ...state,
+          instructionPointer: state.instructionPointer + +arg,
+        };
+      default:
+        throw "unrecognised instruction";
     }
+  }
+
+  const initialState: ProgramState & {
+    visitedInstructions: { [address: number]: boolean };
+  } = {
+    accumulator: 0,
+    instructionPointer: 0,
+    visitedInstructions: {},
+    halt: "NO",
+  };
+
+  function loopGuard(
+    instruction: (state: ProgramState) => ProgramState,
+    state: TrackingProgramState
+  ): TrackingProgramState {
     if (state.visitedInstructions[state.instructionPointer]) {
       return { ...state, halt: "ERR" };
     }
@@ -45,45 +75,39 @@ export function day8() {
       ...state.visitedInstructions,
       [state.instructionPointer]: true,
     };
-    const [opCode, arg] = program[state.instructionPointer].split(" ");
-    switch (opCode) {
-      case "nop":
-        return {
-          ...state,
-          instructionPointer: 1 + state.instructionPointer,
-          visitedInstructions,
-        };
-      case "acc":
-        return {
-          ...state,
-          accumulator: state.accumulator + +arg,
-          instructionPointer: 1 + state.instructionPointer,
-          visitedInstructions,
-        };
-      case "jmp":
-        return {
-          ...state,
-          instructionPointer: state.instructionPointer + +arg,
-          visitedInstructions,
-        };
-      default:
-        throw "unrecognised instruction";
-    }
+
+    return { ...instruction(state), visitedInstructions };
   }
 
-  const initialState: ProgramState = {
-    accumulator: 0,
-    instructionPointer: 0,
-    visitedInstructions: {},
-    halt: "NO",
-  };
-
-  function evaluate(program: string[], state: ProgramState): ProgramState {
-    if (state.halt !== "NO") {
-      return state;
+  function terminationGuard(
+    instruction: (state: ProgramState) => ProgramState,
+    state: ProgramState,
+    programLength: number
+  ): ProgramState {
+    if (state.instructionPointer === programLength) {
+      return { ...state, halt: "OK" };
     }
-    const nextState = instruction(program, state);
-    return evaluate(program, nextState);
+    return instruction(state);
+  }
+
+  function evaluate(
+    program: string[],
+    state: TrackingProgramState
+  ): TrackingProgramState {
+    return doWhile(
+      (st) =>
+        loopGuard(
+          (s) =>
+            terminationGuard(
+              (s) => executeInstruction(program, s),
+              s,
+              program.length
+            ),
+          st
+        ),
+      (st) => st.halt === "NO",
+      state
+    );
   }
 
   const program = day8Program.split("\n");
