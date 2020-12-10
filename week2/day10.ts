ebc42958c7c4d93140b11a4c5a37898dbe5f57c2
Chris May <chris.may@bjss.com>
2020-12-10 23:50:57 +0000

    Tidy up

--- week2/day10.ts
+++ week2/day10.ts
@@ -4,19 +4,16 @@ import { day10Adapters, longerAdapterList, shortAdapterList } from "./day10_inpu
 type NumberMap = { [k: number]: boolean };
 type PathCounter = (n: number, source: NumberMap, countPaths: PathCounter) => number;
 
-export function day10() {
+export function day10(): void {
   const testDataOrdered = orderBy(day10Adapters.split("\n").map(toNumber));
   const counts = testDataOrdered.reduce(
     (acc, n) => {
       const diff = n - acc.prev;
-      if (diff === 1) return { ...acc, prev: n, ones: acc.ones + 1 };
-      if (diff === 2) return { ...acc, prev: n, twos: acc.twos + 1 };
-      if (diff === 3) return { ...acc, prev: n, threes: acc.threes + 1 };
-      throw "Invalid input";
+      return { ...acc, prev: n, [diff]: acc[diff] + 1 };
     },
-    { ones: 0, twos: 0, threes: 0, prev: 0 }
+    { 1: 0, 3: 0, prev: 0 } as { [k: number]: number; prev: number }
   );
-  console.log("Day 10 Part 1:", counts.ones * (counts.threes + 1)); // 1 three for the device's own adapter
+  console.log("Day 10 Part 1:", counts[1] * (counts[3] + 1)); // add 1 three for the device's own adapter
 
   const countPathsTo: PathCounter = memoize((n, source, countPaths) => {
     if (n === 0) return 1;
