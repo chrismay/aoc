a3e8d6d0a330069a473504a7ab97d07735f940c9
Chris May <chris.may@bjss.com>
2020-12-19 14:35:18 +0000

    Day 19

--- week3/day19.ts
+++ week3/day19.ts
@@ -1,5 +1,5 @@
-import { every, fromPairs } from "lodash";
-import { puzzle } from "./day19_input";
+import { every, fromPairs, some, uniq } from "lodash";
+import { loopPuzzle, puzzle } from "./day19_input";
 type RulesMap = { [n: number]: string[] };
 
 function parseInput(inputData: string) {
@@ -56,27 +56,100 @@ function refineList(rulesList: string[], resolved: RulesMap) {
   return { resolved, remaining };
 }
 
+function isSelfReferentialRule(ruleLine: string) {
+  const [ruleNum, rule] = ruleLine.split(":");
+  const ruleElements = rule.split(" ");
+  return some(ruleElements, (e) => e === ruleNum);
+}
+
+function getSubRules(ruleLine: string) {
+  const [ruleNum, rule] = ruleLine.split(":");
+  return rule
+    .split(" ")
+    .filter((ch) => ch.length > 0)
+    .filter((ch) => isFinite(+ch))
+    .filter((ch) => ch !== ruleNum);
+}
+
+function match(input: string, resolved: RulesMap, ruleLen: number) {
+  // must end in a string of 31s. Then there must be at least one more 42 in front
+
+  const match42 = fromPairs(resolved[42].map((s) => [s, true]));
+  const match31 = fromPairs(resolved[31].map((s) => [s, true]));
+  let remainder = input;
+  // eat off the 31s from the end
+  const endRegex = new RegExp("(.*)([a-z]{" + ruleLen + "})$");
+
+  let c31 = 0;
+  let eating31s = true;
+  while (remainder.length > 0 && eating31s) {
+    const [, prefix, ending] = remainder.match(endRegex) || [];
+    if (match31[ending] === undefined) {
+      eating31s = false;
+    } else {
+      //console.log("Ate a 31 from the end");
+      c31++;
+      remainder = prefix;
+    }
+  }
+  if (c31 === 0) {
+    return false;
+  }
+  //  console.log(`at ${c31} 31s from the end`);
+
+  //now eat off the 42s...
+  let c42 = 0;
+  let eating42s = true;
+  while (remainder.length > 0 && eating42s) {
+    const beginRegex = new RegExp("^([a-z]{" + ruleLen + "})(.*)");
+
+    const [, next, rest] = remainder.match(beginRegex) || [];
+    remainder = rest;
+    if (match42[next]) {
+      c42++;
+    } else {
+      eating42s = false;
+    }
+    //    console.log(`ate ${c42} 42s`);
+  }
+  if (remainder.length > 0) {
+    //console.log(`${input} had a remainder ${remainder}`);
+    return false;
+  }
+  if (c42 - c31 < 1) {
+    //console.log(`${input} had a mismatch :42 {}`);
+    return false;
+  }
+  return true;
+}
+
 export function day19() {
-  console.log("Day 19 part 1:");
-
-  const [ruleInput, messages] = puzzle.split("\n\n");
-
+  const [ruleInput1, messages1] = puzzle.split("\n\n");
   /// FIRST PASS
-  var { resolved, remaining } = parseInput(ruleInput);
-
+  var { resolved, remaining } = parseInput(ruleInput1);
   //// REPEATED PASS
   while (remaining.length > 0) {
     const r = refineList(remaining, resolved);
     resolved = r.resolved;
     remaining = r.remaining;
   }
-  console.log(resolved[0][0]);
-  console.log(remaining);
-
   const allowedMessages = fromPairs(resolved[0].map((s) => [s, true]));
+  console.log("Day 19 part 1:", messages1.split("\n").filter((m) => allowedMessages[m]).length);
 
-  console.log("built map");
-  console.log("Day 19 part 1:", messages.split("\n").filter((m) => allowedMessages[m]).length);
+  const [ruleInput, messages] = loopPuzzle.split("\n\n");
+  const recursionInputs = uniq(ruleInput.split("\n").filter(isSelfReferentialRule).flatMap(getSubRules));
+  ///// FIRST PASS
+  var { resolved, remaining } = parseInput(ruleInput);
+  //// REPEATED PASS
+  while (some(recursionInputs, (n) => resolved[+n] === undefined)) {
+    const r = refineList(remaining, resolved);
+    resolved = r.resolved;
+    remaining = r.remaining;
+  }
+  //console.log(resolved, remaining);
+
+  const matches = messages.split("\n").filter((m) => match(m, resolved, 8));
+  console.log("Day 19 part 2:", matches.length);
 }
 
 day19();
