8fe568279e82f5eba6197acb925ceaaca3577f35
Chris May <chris.may@bjss.com>
2020-12-20 21:25:13 +0000

    Day 20 part 2 wip

--- week3/day20.ts
+++ week3/day20.ts
@@ -1,53 +1,71 @@
-import { fromPairs, mapValues, reverse, toPairs, uniq, values } from "lodash";
+import { mapValues, reverse, values } from "lodash";
 import { puzzleInput } from "./day20_input";
+
+type MatchedEdge = { edge: string; matches: number[] };
+type MatchedEdges = { top: MatchedEdge; left: MatchedEdge; right: MatchedEdge; bottom: MatchedEdge };
+type TileEdges = { id: number; tile: string[]; edges: MatchedEdges; transformation: string };
+
 const strRev = (str: string) => reverse(str.split("")).join("");
 
-function getEdges(tile: string[]) {
-  const top = tile[0];
-  const bottom = tile[tile.length - 1];
-  const left = tile.map((l) => l[0]).join("");
-  const right = tile.map((l) => l[l.length - 1]).join("");
+function getEdges(tile: string[]): MatchedEdges {
+  const top = { edge: tile[0], matches: [] };
+  const bottom = { edge: tile[tile.length - 1], matches: [] };
+  const left = { edge: tile.map((l) => l[0]).join(""), matches: [] };
+  const right = { edge: tile.map((l) => l[l.length - 1]).join(""), matches: [] };
   return { top, bottom, left, right };
 }
 
-function parseInput(input: string) {
+function parseInput(input: string): TileEdges[] {
   const tiles = input.split("\n\n");
-  return fromPairs(
-    tiles.map((tile) => {
-      const [idLine, ...rest] = tile.split("\n");
-      const [, id] = idLine.match(/Tile ([0-9]+):/) || [];
-      return [id, { tile: rest, edges: getEdges(rest) }];
-    })
-  );
+  return tiles.map((tile) => {
+    const [idLine, ...rest] = tile.split("\n");
+    const [, id] = idLine.match(/Tile ([0-9]+):/) || [];
+    return { id: +id, tile: rest, edges: getEdges(rest), transformation: "" };
+  });
 }
-type MatchedEdge = { edge: string; matches: number[] };
-type MatchedEdges = { top: MatchedEdge; left: MatchedEdge; right: MatchedEdge; bottom: MatchedEdge };
-function transformToMatchRightEdge(edge: string, tile: { id: number; edges: MatchedEdges }) {
+
+function transformToMatchBottonEdge(edge: string, tile: TileEdges): TileEdges {
   let transf = tile;
-  console.log(tile);
+  // console.log(tile);
   let rotations = 0;
-  let transform = "";
+  while (transf.edges.top.edge !== edge && rotations < 4) {
+    transf = rotate(transf);
+    rotations++;
+  }
+  if (transf.edges.top.edge !== edge) {
+    transf = flip(transf);
+  }
+  while (transf.edges.top.edge !== edge && rotations < 8) {
+    transf = rotate(transf);
+    rotations++;
+  }
+  // console.log("Got a match", transf.transformation);
+  return transf;
+}
+
+function transformToMatchRightEdge(edge: string, tile: TileEdges): TileEdges {
+  let transf = tile;
+  // console.log(tile);
+  let rotations = 0;
   while (transf.edges.left.edge !== edge && rotations < 4) {
     transf = rotate(transf);
-    transform = transform + "R";
     rotations++;
   }
   if (transf.edges.left.edge !== edge) {
     transf = flip(transf);
-    transform = transform + "F";
   }
-  while (transf.edges.left.edge !== edge && rotations < 4) {
+  while (transf.edges.left.edge !== edge && rotations < 8) {
     transf = rotate(transf);
-    transform = transform + "R";
     rotations++;
   }
-  console.log("Got a match", transform);
-  return { tile: transf, transform };
+  // console.log("Got a match", transf.transformation);
+  return transf;
 }
 
-function flip(tile: { id: number; edges: MatchedEdges }): { id: number; edges: MatchedEdges } {
+function flip(tile: TileEdges): TileEdges {
   return {
-    id: tile.id,
+    ...tile,
+    transformation: tile.transformation + "R",
     edges: {
       top: tile.edges.bottom,
       bottom: tile.edges.top,
@@ -57,9 +75,10 @@ function flip(tile: { id: number; edges: MatchedEdges }): { id: number; edges: M
   };
 }
 
-function rotate(tile: { id: number; edges: MatchedEdges }): { id: number; edges: MatchedEdges } {
+function rotate(tile: TileEdges): TileEdges {
   return {
-    id: tile.id,
+    ...tile,
+    transformation: tile.transformation + "F",
     edges: {
       top: { edge: strRev(tile.edges.left.edge), matches: tile.edges.left.matches },
       bottom: { edge: strRev(tile.edges.right.edge), matches: tile.edges.right.matches },
@@ -75,75 +94,66 @@ export function day20(): void {
   // console.log("Day 20", tiles);
 
   const edgeMap: { [edge: string]: number[] } = {};
-  toPairs(tiles).forEach(([id, { edges }]) => {
-    values(edges).forEach((edge) => {
-      const reversed: string = strRev(edge);
+  tiles.forEach((t) => {
+    values(t.edges).forEach((edge) => {
+      const reversed: string = strRev(edge.edge);
 
-      edgeMap[edge] = [...(edgeMap[edge] || []), +id];
-      edgeMap[reversed] = [...(edgeMap[reversed] || []), +id];
+      edgeMap[edge.edge] = [...(edgeMap[edge.edge] || []), t.id];
+      edgeMap[reversed] = [...(edgeMap[reversed] || []), t.id];
     });
   });
 
-  const matchingEdges = toPairs(tiles).map(([id, { edges }]) => {
-    const matches = uniq(values(edges).flatMap((e) => edgeMap[e].filter((otherId) => +id !== otherId)));
-    return { id, edges, matches };
-  });
-
-  //   const corners = matchingEdges.filter(({ id, edges, matches }) => matches.length === 2);
-  //   console.log(
-  //     "Day 20 Part 1:",
-  //     corners.reduce((acc, { id }) => acc * +id, 1)
-  //   );
-
-  const xx = toPairs(tiles).map(([id, { edges }]) => {
+  const tilesWithMatches: TileEdges[] = tiles.map((tile) => {
     return {
-      id,
-      edges: mapValues(edges, (e) => ({ edge: e, matches: edgeMap[e].filter((otherId) => +id !== otherId) })),
+      ...tile,
+      edges: mapValues(tile.edges, (e) => ({
+        ...e,
+        matches: edgeMap[e.edge].filter((otherId) => tile.id !== otherId),
+      })),
     };
   });
-  const tilesWithEdgeMatches = fromPairs(xx.map(({ id, edges }) => [+id, edges]));
 
-  const corners = xx.filter(({ id, edges }) => values(edges).flatMap((v) => v.matches).length === 2);
-  corners.forEach((c) => {
-    console.log(c.id);
-    toPairs(c.edges).forEach(([n, detail]) => {
-      if (detail.matches.length > 0) {
-        console.log(n, detail);
+  const corners = tilesWithMatches.filter((t) => {
+    return values(t.edges).flatMap((e) => e.matches).length === 2;
+  });
+
+  const topLeft = tilesWithMatches.find((t) => {
+    return t.edges.top.matches.length === 0 && t.edges.left.matches.length === 0;
+  })!!;
+
+  console.log(topLeft);
+
+  tilesWithMatches.forEach((t) => {
+    values(t.edges).forEach((edge) => {
+      if (edge.matches.length > 1) {
+        throw "Edge Ambiguous";
       }
     });
   });
-
-  let currentTiles: any = corners.filter((corner) => corner.edges.right.matches.length > 0)!!;
-  let currentTile = currentTiles[0];
-  let matchedTiles: any[] = [currentTile];
-  while (matchedTiles.length < width) {
-    console.log(`current tile ID is ${currentTile.id}`);
-    console.log(`possible edge matches are ${edgeMap[currentTile.edges.right.edge]}`);
-    const matchTileIds = matchedTiles.map((mt) => +mt.id);
-    const nextTiles = edgeMap[currentTile.edges.right.edge].filter(
-      (match) => matchTileIds.find((m) => m === match) === undefined
-    );
-    console.log(`filtered matches are ${nextTiles}`);
-    switch (nextTiles.length) {
-      case 0:
-        throw "Failed to find an edge match";
-      case 1:
-        console.log(`Match for ${currentTile.edges.right.edge} is ${nextTiles[0]} `);
-        const transformed = transformToMatchRightEdge(currentTile.edges.right.edge, {
-          id: nextTiles[0],
-          edges: tilesWithEdgeMatches[nextTiles[0]],
-        });
-        currentTile = transformed.tile;
-        matchedTiles.push(currentTile);
-        break;
-      default:
-        throw `Multiple matches (${nextTiles.length}) for ${currentTile.edges.right.edge} - picking the first??`;
-    }
+  let topRow: TileEdges[] = [topLeft];
+  while (topRow.length < width) {
+    const curr = topRow[topRow.length - 1];
+    const nextId = curr.edges.right.matches[0];
+    const next = transformToMatchRightEdge(curr.edges.right.edge, tilesWithMatches.find((t) => t.id === nextId)!!);
+    topRow = [...topRow, next];
   }
-  console.log(
-    "First row: ",
-    matchedTiles.map((m) => +m.id)
-  );
+  console.log(topRow.map((t) => t.id));
+
+  let picture: TileEdges[][] = [topRow];
+  // now work downwards
+  topRow.forEach((tile, index) => {
+    let col: TileEdges[] = [tile];
+    while (col.length < width) {
+      const curr = col[col.length - 1];
+      const nextId = curr.edges.bottom.matches[0];
+      const next = transformToMatchBottonEdge(curr.edges.bottom.edge, tilesWithMatches.find((t) => t.id === nextId)!!);
+      col = [...col, next];
+    }
+    console.log(
+      "col " + index,
+      col.map((t) => t.id)
+    );
+  });
 }
 
 day20();
