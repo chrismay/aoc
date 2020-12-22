f3fb0201b6caeaae904f49bbe4c94f7aa6a08b5d
Chris May <chris.may@bjss.com>
2020-12-22 20:29:32 +0000

    renames

--- week4/day22.ts
+++ week4/day22.ts
@@ -6,50 +6,51 @@ type Deck = number[];
 type Game = { p1: Deck; p2: Deck; prevP1: Set<string>; winner?: Player };
 type Player = "p1" | "p2";
 
-function playRound(game: Game, playingRecusiveRule: boolean): Game {
+function playRound(prevRound: Game, playingRecusiveRule: boolean): Game {
   // Check that we aren't looping.
-  const p1Hand = game.p1.join(",");
-  if (game.prevP1.has(p1Hand)) {
+  const p1Hand = prevRound.p1.join(",");
+  if (prevRound.prevP1.has(p1Hand)) {
     // Loop detection defaults the game to P1
-    return { ...game, winner: "p1" };
+    return { ...prevRound, winner: "p1" };
   } else {
-    const prevP1 = new Set([...game.prevP1, p1Hand]);
+    const prevP1 = new Set([...prevRound.prevP1, p1Hand]);
 
-    // construct the input state for the next round
-    const nextGame = { p1: [...game.p1], p2: [...game.p2], prevP1 };
-    const p1Card = notNull(nextGame.p1.shift());
-    const p2Card = notNull(nextGame.p2.shift());
-    if (playingRecusiveRule && nextGame.p1.length >= p1Card && nextGame.p2.length >= p2Card) {
+    // construct the input state for the next round, initially just a clone of the current state
+    const nextRound = { p1: [...prevRound.p1], p2: [...prevRound.p2], prevP1 };
+    const p1Card = notNull(nextRound.p1.shift());
+    const p2Card = notNull(nextRound.p2.shift());
+    if (playingRecusiveRule && nextRound.p1.length >= p1Card && nextRound.p2.length >= p2Card) {
       const recursiveGame: Game = {
-        p1: [...nextGame.p1].slice(0, p1Card),
-        p2: [...nextGame.p2].slice(0, p2Card),
+        p1: [...nextRound.p1].slice(0, p1Card),
+        p2: [...nextRound.p2].slice(0, p2Card),
         prevP1: new Set(),
       };
       const result = playGame(recursiveGame, true);
       if (result.winner === "p1") {
-        nextGame.p1.push(p1Card);
-        nextGame.p1.push(p2Card);
+        nextRound.p1.push(p1Card);
+        nextRound.p1.push(p2Card);
       } else {
-        nextGame.p2.push(p2Card);
-        nextGame.p2.push(p1Card);
+        nextRound.p2.push(p2Card);
+        nextRound.p2.push(p1Card);
       }
     } else {
       //    Non-recursive; play normally
       if (p1Card > p2Card) {
-        nextGame.p1.push(p1Card);
-        nextGame.p1.push(p2Card);
+        nextRound.p1.push(p1Card);
+        nextRound.p1.push(p2Card);
       } else {
-        nextGame.p2.push(p2Card);
-        nextGame.p2.push(p1Card);
+        nextRound.p2.push(p2Card);
+        nextRound.p2.push(p1Card);
       }
     }
-    if (nextGame.p1.length === 0) {
-      return { ...nextGame, winner: "p2" };
+    // determine whether anyone's won yet.
+    if (nextRound.p1.length === 0) {
+      return { ...nextRound, winner: "p2" };
+    } else if (nextRound.p2.length === 0) {
+      return { ...nextRound, winner: "p1" };
+    } else {
+      return nextRound;
     }
-    if (nextGame.p2.length === 0) {
-      return { ...nextGame, winner: "p1" };
-    }
-    return nextGame;
   }
 }
 
@@ -84,3 +85,4 @@ export function day22(): void {
   recursiveGame = playGame(recursiveGame, true);
   console.log("Day 22 Part 2:", scoreGame(recursiveGame));
 }
+day22();
