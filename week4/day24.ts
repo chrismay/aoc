0b6c31c227d2d105d1aff481e29dfd889369c38a
Chris May <chris.may@bjss.com>
2020-12-24 22:47:29 +0000

    tidy

--- week4/day24.ts
+++ week4/day24.ts
@@ -1,7 +1,10 @@
-import { countBy, keys, range, toNumber, toPairs, uniq, values } from "lodash";
+import { countBy, keys, pickBy, range, toNumber, uniq, values } from "lodash";
 import { tileInput } from "./day24_input";
 
 type Coord = { x: number; y: number; z: number };
+const origin: Coord = { x: 0, y: 0, z: 0 };
+
+// cubic co-ordinates for hexagonal grids.
 const allDirections: { [dir: string]: Coord } = {
   e: { x: 1, y: -1, z: 0 },
   w: { x: -1, y: 1, z: 0 },
@@ -11,7 +14,6 @@ const allDirections: { [dir: string]: Coord } = {
   se: { x: 0, y: -1, z: +1 },
 } as const;
 
-type Move = keyof typeof allDirections;
 type Pattern = Set<string>;
 
 function add(c1: Coord, c2: Coord): Coord {
@@ -27,16 +29,8 @@ function s2c(s: string): Coord {
 
 const inputMatcher = new RegExp(`(${keys(allDirections).join("|")})`, "g");
 
-function parseMoves(input: string): Move[] {
-  return input.match(inputMatcher) || [];
-}
-
-function applyMove(from: Coord, move: Move): Coord {
-  return add(from, allDirections[move]);
-}
-
-function getOffset(moves: Move[]) {
-  return moves.reduce(applyMove, { x: 0, y: 0, z: 0 });
+function toMoves(input: string): Coord[] {
+  return (input.match(inputMatcher) || []).map((dir) => allDirections[dir]);
 }
 
 function getSurrounding(position: Coord): Coord[] {
@@ -50,37 +44,31 @@ function getNextRoundPieces(pattern: Pattern) {
   return uniq([...pattern].map(s2c).flatMap(getSurrounding));
 }
 
-function determinePieceColour(pattern: Pattern, pos: Coord): boolean {
-  const count = countSurroundingBlackPieces(pattern, pos);
-  const currentlyBlack = pattern.has(c2s(pos));
-  if (currentlyBlack && (count === 0 || count > 2)) {
-    return false;
-  }
-  if (!currentlyBlack && count === 2) {
-    return true;
-  }
-  return currentlyBlack;
+function becomesBlackTile(pattern: Pattern) {
+  return function (pos: Coord): Coord[] {
+    const count = countSurroundingBlackPieces(pattern, pos);
+    const currentlyBlack = pattern.has(c2s(pos));
+    if (currentlyBlack && (count === 0 || count > 2)) {
+      return [];
+    }
+    if (!currentlyBlack && count === 2) {
+      return [pos];
+    }
+    return currentlyBlack ? [pos] : [];
+  };
 }
 
 function nextPattern(pattern: Pattern): Pattern {
-  const newState = getNextRoundPieces(pattern)
-    .map((pos) => [c2s(pos), determinePieceColour(pattern, pos)])
-    .filter(([, v]) => v)
-    .map(([k]) => k as string);
-  return new Set(newState);
+  const newState = getNextRoundPieces(pattern).flatMap(becomesBlackTile(pattern));
+  return new Set(newState.map(c2s));
 }
 
-export function day24() {
-  const modifications = tileInput.split("\n").map((moves) => c2s(getOffset(parseMoves(moves))));
-  const blackPieces = toPairs(countBy(modifications))
-    .filter(([, v]) => v % 2 === 1) //ignore any piece which is changed and then changed back again.
-    .map(([k]) => k);
-  const pattern: Pattern = new Set(blackPieces);
+export function day24(): void {
+  const modifications = tileInput.split("\n").map((moveNames) => c2s(toMoves(moveNames).reduce(add, origin)));
+  const blackPieces = keys(pickBy(countBy(modifications), (count) => count % 2 === 1));
 
-  console.log("Day 24 Part 1:", pattern.size);
+  console.log("Day 24 Part 1:", blackPieces.length);
 
-  const finalPattern = range(0, 100).reduce(nextPattern, pattern);
+  const finalPattern = range(0, 100).reduce(nextPattern, new Set(blackPieces));
   console.log("Day 24 Part 2:", finalPattern.size);
 }
-
-day24();
